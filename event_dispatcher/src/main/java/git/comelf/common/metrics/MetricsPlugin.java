5f33c0702b883523f4d69907f2acf98c47b1bfb1
comelf <leebyungwoo@gmail.com>
2022-01-13 10:14:08 +0900

    remove all metrics

--- event_dispatcher/src/main/java/git/comelf/common/metrics/MetricsPlugin.java
+++
@@ -1,9 +0,0 @@
-package git.comelf.common.metrics;
-
-public interface MetricsPlugin {
-    /**
-     * Initialize the plugin
-     * @param conf  the configuration object for the plugin
-     */
-    void init();
-}
--- event_dispatcher/src/main/java/git/comelf/common/metrics/MetricsSource.java
+++
@@ -1,7 +0,0 @@
-package git.comelf.common.metrics;
-
-public interface MetricsSource {
-
-    void getMetrics(MetricsCollector collector, boolean all);
-
-}
