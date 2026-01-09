24b1048b54aa2c6a859ed69343c2f07c1d3a7ca9
Byung-woo Lee <comelf@users.noreply.github.com>
2026-01-09 19:04:15 +0900

    Update AESUtil.java

--- utils/src/main/java/com/github/comelf/AESUtil.java
+++ utils/src/main/java/com/github/comelf/AESUtil.java
@@ -13,60 +13,87 @@ import java.util.Arrays;
 
 public class AESUtil {
 
-    private static final int KEY_LEN = 32;
-    private static final int IV_LEN = 16;
+    private static final int IV_LENGTH = 16;
+    public static final int AES256_BIT = 32;
 
-    public static String decrypt(String ciphertext, String passphrase) {
-        try {
-            byte[] ctBytes = Base64.decodeBase64(ciphertext.getBytes("UTF-8"));
+    private final int keyLength;
+    private final Key keySpec;
+    private final IvParameterSpec iv;
 
-            byte[] saltBytes = Arrays.copyOfRange(ctBytes, 8, 16);
-
-            byte[] ciphertextBytes = Arrays.copyOfRange(ctBytes, 16, ctBytes.length);
-
-            byte[] passBytes = passphrase.getBytes("UTF-8");
-
-            Cipher cipher2 = getCipher(passBytes, saltBytes);
-            return new String(cipher2.doFinal(ciphertextBytes));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public AESUtil(String key) {
+        this(key, AES256_BIT);
     }
 
-    private static Cipher getCipher(byte[] passBytes, byte[] saltBytes) throws Exception {
-        byte[] key = new byte[KEY_LEN];
-        byte[] iv = new byte[IV_LEN];
+    public AESUtil(String key, int length) {
+        this(Objects.requireNonNull(key).getBytes(StandardCharsets.UTF_8), length);
+    }
 
-        byte[] derivedBytes = new byte[48];
-        int numberOfDerivedWords = 0;
-        byte[] block = null;
-        MessageDigest hasher = MessageDigest.getInstance("MD5");
-        while (numberOfDerivedWords < 12) {
-            if (block != null) {
-                hasher.update(block);
-            }
-            hasher.update(passBytes);
-            // Salting
-            block = hasher.digest(saltBytes);
-            hasher.reset();
-            System.arraycopy(block, 0, derivedBytes, numberOfDerivedWords * 4, Math.min(block.length, (12 - numberOfDerivedWords) * 4));
-            numberOfDerivedWords += block.length / 4;
+    public AESUtil(byte[] key, int length) {
+        this(key, length, null);
+    }
+
+    public AESUtil(byte[] key, int length, byte[] iv) {
+        Objects.requireNonNull(key);
+        this.keyLength = length;
+        this.keySpec = makeKey(key);
+        if (iv != null) {
+            this.iv = makeIv(iv);
+        } else {
+            this.iv = makeIv(key);
         }
-        System.arraycopy(derivedBytes, 0, key, 0, KEY_LEN);
-        System.arraycopy(derivedBytes, KEY_LEN, iv, 0, IV_LEN);
+    }
 
+    private IvParameterSpec makeIv(byte[] key) {
+        byte[] iv = new byte[IV_LENGTH];
+        int keyLen = key.length;
+        if (keyLen < IV_LENGTH) {
+            int r = (int) Math.ceil(IV_LENGTH / (double) keyLen);
+            byte[] temp = new byte[keyLen * r];
+            for (int i = 0; i < r; i++) {
+                System.arraycopy(key, 0, temp, i * keyLen, keyLen);
+            }
+            key = temp;
+        }
+        if (key.length < IV_LENGTH) {
+            throw new IllegalArgumentException("Invalid iv length");
+        }
+        System.arraycopy(key, 0, iv, 0, IV_LENGTH);
+        return new IvParameterSpec(iv);
+    }
 
-        // 복호화
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(iv));
-        return cipher;
+    private Key makeKey(byte[] key) {
+        byte[] pass = new byte[keyLength];
+        int keyLen = key.length;
+        if (keyLen == 0) {
+            throw new IllegalArgumentException("iv key must be not empty.");
+        }
+        if (keyLen < this.keyLength) {
+            int r = (int) Math.ceil(keyLength / (double) keyLen);
+            byte[] temp = new byte[keyLen * r];
+            for (int i = 0; i < r; i++) {
+                System.arraycopy(key, 0, temp, i * keyLen, keyLen);
+            }
+            key = temp;
+        }
+        if (key.length < this.keyLength) {
+            throw new IllegalArgumentException("Invalid key length");
+        }
+        System.arraycopy(key, 0, pass, 0, keyLength);
+        return new SecretKeySpec(pass, "AES");
+    }
+
+    public String encrypt(String str) throws Exception {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+        byte[] encrypted = c.doFinal(str.getBytes(StandardCharsets.UTF_8));
+        return new String(Base64.encodeBase64(encrypted));
+    }
+
+    public String decrypt(String str) throws Exception {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.DECRYPT_MODE, keySpec, iv);
+        byte[] byteStr = Base64.decodeBase64(str.getBytes());
+        return new String(c.doFinal(byteStr), StandardCharsets.UTF_8);
     }
 
 }
